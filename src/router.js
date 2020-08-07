ad92dcaf8e67d1b258128ebc71d89dd0b3334cfe
LRS4 <42714431+LRS4@users.noreply.github.com>
2020-08-07 18:28:08 +0100

    Make vue use buefy

--- src/router.js
+++ src/router.js
@@ -2,8 +2,11 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 import Api from './views/Api.vue'
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
 
 Vue.use(Router)
+Vue.use(Buefy)
 
 export default new Router({
   routes: [
