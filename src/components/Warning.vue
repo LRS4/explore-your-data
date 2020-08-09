b9bef32373453f87cf093ff151e7cf2cdf4e95bf
LRS4 <42714431+LRS4@users.noreply.github.com>
2020-08-09 20:07:58 +0100

    Add warning component

---
+++ src/components/Warning.vue
@@ -0,0 +1,35 @@
+/* eslint-disable semi */
+<template>
+  <section>
+    <div class="warning">
+        <div class="content">
+            <p><b-icon class="alert-circle" icon="alert-circle" size="is-medium"></b-icon> {{ message }}</p>
+        </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'warning',
+  props: {
+    message: String
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.warning {
+    margin-bottom: 30px;
+}
+
+.content {
+    margin: 10px;
+    color: black;
+    font-weight: bold;
+}
+
+.alert-circle {
+    vertical-align: bottom !important;
+}
+</style>
--- src/views/Upload.vue
+++ src/views/Upload.vue
@@ -7,9 +7,10 @@
           v-if="showError"
           type="error"
           title="There is a problem"
-          message="The file must be in CSV (comma-separated values) format"
+          message="The file must be in CSV (comma-separated values) format."
         />
-        <FileUploader @showError="showError = $event" @dataValid="isButtonDisabled = false" />
+        <Warning message="Only upload data which is not sensitive, or has been anonymised." />
+        <FileUploader @showError="showError = $event" @dataValid="isButtonDisabled = $event" />
         <b-button
           class="is-success button has-text-weight-bold"
           size="is-medium"
@@ -27,14 +28,14 @@
 <script>
 // @ is an alias to /src
 import FileUploader from "@/components/FileUploader.vue";
-import Help from "@/components/Help.vue";
+import Warning from "@/components/Warning.vue";
 import Notification from "@/components/Notification.vue";
 
 export default {
   name: "upload",
   components: {
     FileUploader,
-    Help,
+    Warning,
     Notification,
   },
   data() {
@@ -48,6 +49,10 @@ export default {
     submitData() {
       console.log("Submitting data...");
       this.isButtonLoading = true;
+
+      // initial check - is the data valid?
+
+      // if so - process it and continue
     },
   },
 };
