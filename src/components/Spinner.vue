db161ec73fc28d68c36be18f8350e79fc3c66cdc
LRS4 <42714431+LRS4@users.noreply.github.com>
2020-08-12 20:18:12 +0100

    Add spinner component

---
+++ src/components/Spinner.vue
@@ -0,0 +1,20 @@
+<template>
+  <section>
+      <b-loading :is-full-page="isFullPage" :active.sync="isLoading" :can-cancel="false">
+        <b-icon pack="fas" icon="spinner" size="is-large" custom-class="fa-pulse"></b-icon>
+      </b-loading>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isFullPage: true
+    };
+  },
+  props: {
+    isLoading: Boolean
+  }
+};
+</script>
