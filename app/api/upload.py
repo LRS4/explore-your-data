33bd49b0f70ebc3c8fce2e886d6f6dbd5d5bcf85
LRS4 <42714431+LRS4@users.noreply.github.com>
2020-08-17 17:18:22 +0100

    Upgrade deprecated werkzeug call

--- app/api/upload.py
+++ app/api/upload.py
@@ -6,7 +6,7 @@ https://flask-restx.readthedocs.io/en/latest/quickstart.html
 from datetime import datetime
 from flask import request
 from flask_restx import Resource
-from werkzeug import secure_filename
+from werkzeug.utils import secure_filename
 
 from .security import require_auth
 from . import api_rest
