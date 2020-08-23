5a537f2d35deec698009181b3a36dc8645129775
LRS4 <42714431+LRS4@users.noreply.github.com>
2020-08-23 18:20:04 +0100

    Set google cloud storage to work with env vars

--- app/services/file_service.py
+++ app/services/file_service.py
@@ -1,24 +1,30 @@
 import io
 import os
-import requests
-import numpy as np
-import pandas as pd
 import time
 from werkzeug.utils import secure_filename
+from .config import bucket
+
+import numpy as np
+import pandas as pd
 
 
 def upload_file(file, session_id):
-    """ Uploads a file to blob storage """
+    """ Uploads a file to blob storage bucket """
     filename = secure_filename(str(session_id) + '.csv')
     file.save(filename)
 
 
+def list_files_in_bucket():
+    for blob in bucket.list_blobs():
+        print(blob)
+
+
 def read_file(filename) -> pd.DataFrame:
-    """ Reads a file to blob storage """
+    """ Reads a file from blob storage bucket """
     return pd.read_csv(filename + ".csv")
 
 
 def remove_file(filename):
-    """ Removes a file to blob storage """
+    """ Removes a file from blob storage bucket """
     time.sleep(3)
     os.remove(filename + ".csv")
