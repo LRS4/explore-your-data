fab11d212480e222f80258a6c31dec8a37299633
LRS4 <42714431+LRS4@users.noreply.github.com>
2020-08-25 19:04:46 +0100

    Add comments to utils

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,9 @@
+/**
+ * Retrieves column names from dataset.
+ *
+ * @param {Object} data
+ * @return {Object} Object containing dataset rows.
+ */
 export const getDatasetColumns = (data) => {
   let columns = Object.keys(data);
   return columns.map(name => (
@@ -8,8 +14,15 @@ export const getDatasetColumns = (data) => {
   ));
 }
 
+/**
+ * Transforms dataset into an object of row-data.
+ *
+ * @param {Object} data
+ * @return {Object} Object containing dataset rows.
+ */
 export const getDatasetRows = (data) => {
   let rows = [];
+  console.log(data);
   let numberOfRows = getRowCount(data);
   for (let i = 0; i < numberOfRows; i++) {
     let row = {};
@@ -21,6 +34,12 @@ export const getDatasetRows = (data) => {
   return rows;
 }
 
+/**
+ * Counts the rows in each column and returns max count
+ * 
+ * @param {Object} data 
+ * @return {Number} Count of rows
+ */
 export const getRowCount = (data) => {
   let rowsInColumns = []; 
   for (let col in data) {
