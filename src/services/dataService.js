3e4d88df698e303a9f1fae53925ff23b40a49212
LRS4 <42714431+LRS4@users.noreply.github.com>
2020-09-05 14:04:33 +0100

    Remove blur effect from placeholder images

--- src/services/dataService.js
+++ src/services/dataService.js
@@ -2,13 +2,12 @@ import $axios from '../backend';
 
 export default {
   /**
-   * Retrieves an description of the dataset from pandas describe method
+   * Retrieves a description of the dataset from pandas describe method
    * @return {Object} The dataset description object.
    */
   getDataSummary() {
     return $axios.post(`data/describe`, { sessionId: String(sessionStorage.sessionId) })
       .then(response => {
-        console.log("data", response.data);
         return JSON.parse(response.data);
       })
   }
