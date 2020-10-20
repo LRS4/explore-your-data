c9e0b8122a6f3acb331326bb43fb7312f7b8605d
LRS4 <42714431+LRS4@users.noreply.github.com>
2020-10-20 19:48:09 +0100

    Add further unit tests for plot service

--- tests/test_plot_service.py
+++ tests/test_plot_service.py
@@ -15,7 +15,7 @@ def client():
     return app.test_client()
 
 
-def test_get_distribution_plot(client):
+def test_get_distribution_plot_numeric(client):
     # arrange
     df = pd.read_csv('tests/titanic.csv')
 
@@ -26,12 +26,24 @@ def test_get_distribution_plot(client):
     assert str(distribution_plot) == 'AxesSubplot(0.125,0.11;0.775x0.77)'
 
 
+def test_get_distribution_plot_categorical(client):
+    # arrange
+    df = pd.read_csv('tests/titanic.csv')
+
+    # act
+    distribution_plot = plot_service.get_distribution_plot(df, 'Sex')
+
+    # assert
+    assert str(distribution_plot) == 'AxesSubplot(0.125,0.11;0.775x0.77)'
+
+
 def test_get_scatter_plot(client):
     # arrange
     df = pd.read_csv('tests/titanic.csv')
 
     # act
-    scatter_plot = plot_service.get_scatter_plot(df, 'Sex', 'Age', 'Fare', False)
+    scatter_plot = plot_service.get_scatter_plot(
+        df, 'Sex', 'Age', 'Fare', False)
 
     # assert
     assert str(scatter_plot) == 'AxesSubplot(0.125,0.11;0.775x0.77)'
@@ -43,7 +55,7 @@ def test_convert_continuous_target_dtype(client):
 
     # act
     target_dtype_int = plot_service.convert_continuous_target_dtype(
-      df, 'Survived', 1
+        df, 'Survived', 1
     )
 
     target_dtype_str = plot_service.convert_continuous_target_dtype(
@@ -55,7 +67,7 @@ def test_convert_continuous_target_dtype(client):
     assert type(target_dtype_str) == str
 
 
-def test_get_influencers_plot(client):
+def test_get_influencers_plot_for_numeric_target(client):
     # arrange
     df = pd.read_csv('tests/titanic.csv')
     target_column = 'Survived'
@@ -63,7 +75,24 @@ def test_get_influencers_plot(client):
     filtered_df = df[df[target_column] == target_value]
     x = 'Sex'
 
-    # act 
+    # act
+    influencers_plot = plot = plot_service.get_influencers_plot(
+        df, filtered_df, x, target_column, analysis_type='categorical', is_actuals=0
+    )
+
+    # assert
+    assert str(influencers_plot) == 'AxesSubplot(0.125,0.11;0.775x0.77)'
+
+
+def test_get_influencers_plot_for_categorical_target(client):
+    # arrange
+    df = pd.read_csv('tests/titanic.csv')
+    target_column = 'Sex'
+    target_value = 'male'
+    filtered_df = df[df[target_column] == target_value]
+    x = 'Pclass'
+
+    # act
     influencers_plot = plot = plot_service.get_influencers_plot(
         df, filtered_df, x, target_column, analysis_type='categorical', is_actuals=0
     )
