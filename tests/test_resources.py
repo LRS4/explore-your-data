f34ae732ed08d98ef9ac3c01cd4863ca5df99d45
LRS4 <42714431+LRS4@users.noreply.github.com>
2021-02-02 16:33:32 +0000

    Remove history and license files and extra test

--- tests/test_resources.py
+++ tests/test_resources.py
@@ -30,11 +30,6 @@ def test_resource_one_patch(client):
     assert resp.status_code == 405
 
 
-def test_titanic_data_get(client):
-    resp = client.get('/api/resource/get-titanic-data')
-    assert resp.status_code == 200
-
-
 def test_useful_links_return(client):
     resp = client.get('/api/resource/get-useful-links')
     assert resp.data != None
